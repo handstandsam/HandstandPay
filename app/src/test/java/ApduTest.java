bcc8a02ba5958d29635c42503b0cd9c6fb7c8f66
Sam (@HandstandSam) Edwards <sam@handstandtech.com>
2015-07-29 17:49:17 -0400

    Actually testing something, even though it's lame.

--- app/src/test/java/ApduTest.java
+++ app/src/test/java/ApduTest.java
@@ -2,6 +2,8 @@ import com.handstandsam.handstandpay.apdu.HandstandApduService;
 import com.handstandsam.handstandpay.contstants.Constants;
 import com.handstandsam.handstandpay.util.HexUtil;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 /**
@@ -16,5 +18,6 @@ public class ApduTest {
         byte[] apduResponse = service.getReadRecordResponse(rawSwipeData);
         String apduResponseHex = HexUtil.byteArrayToHex(apduResponse);
         System.out.println(apduResponseHex);
+        Assert.assertNotNull(apduResponseHex);
     }
 }
